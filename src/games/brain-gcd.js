b9f80eb18216d113f50d5e11dc1cf4b4af6d4ad6
Victoria Korotkova <v.korotkova@i-dgtl.ru>
2024-03-16 21:07:43 +0300

    fix

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,12 +1,11 @@
-/* eslint-disable prefer-rest-params */
 import app from '../index.js';
 import getRandomNumber from '../utils/index.js';
 
-function nod() {
-  let x = arguments[0];
-  let y = arguments[0];
-  for (let i = 1; i < arguments.length; i += 1) {
-    y = arguments[i];
+function nod(arr) {
+  let x = arr[0];
+  let y = arr[0];
+  for (let i = 1; i < arr.length; i += 1) {
+    y = arr[i];
     while (x && y) {
       // eslint-disable-next-line no-unused-expressions
       x > y ? (x %= y) : (y %= x);
@@ -19,7 +18,7 @@ function nod() {
 const getAnswers = () => {
   const randomNumber1 = getRandomNumber(1, 10);
   const randomNumber2 = getRandomNumber(1, 10);
-  const correctAnswer = nod(randomNumber1, randomNumber2).toString();
+  const correctAnswer = nod([randomNumber1, randomNumber2]).toString();
 
   console.log(`Question: ${randomNumber1} ${randomNumber2}`);
   return correctAnswer;
